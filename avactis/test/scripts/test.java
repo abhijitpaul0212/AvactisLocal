82e89c28f0e5a7564e91f635a32e894b3905bd3c
abhijitpaul0212 <abhijitpaul0212@gmail.com>
2017-09-25 02:20:48 +0530

    Deleted test.java

--- avactis/test/scripts/test.java
+++ avactis/test/scripts/test.java
@@ -5,9 +5,8 @@ public class test {
 	public static void main(String[] args)
 	{
 		String abc ="#0078#0";
-		String[] text = abc.split("#");
-		System.out.println(text[0]);
-		System.out.println(text[1]);
+	
+		
 	}
 
 }
