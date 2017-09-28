4f1ea2a1fd6ec82145f93ee2caec09780644cebc
abhijitpaul0212 <abhijitpaul0212@gmail.com>
2017-09-29 00:36:34 +0530

    Modified the script

--- avactis/test/scripts/PurchaseItems.java
+++ avactis/test/scripts/PurchaseItems.java
@@ -70,15 +70,12 @@ public class PurchaseItems
 	  By waitForAccountSignInForm= By.cssSelector("*[for='account_sign_in_form_email_id']");
 	  By waitForOrderSearch = By.className("orders_search1");
 	    
-//	  wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.cssSelector("*[for='account_sign_in_form_email_id']"))).equals("E-Mail:");
 	  Waiting.WaitForElement(driver, waitForAccountSignInForm, 30);
-	  //Test login
+	 
 	  driver.findElement(By.id("account_sign_in_form_email_id")).sendKeys("abhijitpaul_02@yahoo.com");
 	  driver.findElement(By.id("account_sign_in_form_passwd_id")).sendKeys("password123");
 	  driver.findElement(By.cssSelector("*[value='Sign In']")).click();
 	  
-	  //Test post login condition
-//	  wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.className("orders_search1"))).equals("ORDERS");
 	  Waiting.WaitForElement(driver, waitForOrderSearch, 30).equals("ORDERS");
 	  assertEquals("ORDERS", driver.findElement(By.className("orders_search1")).getText());
   }
@@ -92,51 +89,50 @@ public class PurchaseItems
 	  String navigateToProduct4 = "Sport>cid45";
 	  String navigateToProduct5 = "Furniture";
 	  
-	  //Navigate to DVD > TV on DVD > House_M_D
-	  //System.out.println("Purchasing first item");
-	  
-	  navigateMenu(driver,2,navigateToProduct1);
-
-	  /*WebElement mainMenu1 = driver.findElement(By.cssSelector("*[href*='DVD'][class='dropdown-toggle']"));
-	  Actions action1 = new Actions(driver);
-	  action1.moveToElement(mainMenu1).build().perform();
-	  driver.findElement(By.cssSelector("*[href*='TV_on_DVD']")).click();*/
+	  try {
+		Thread.sleep(20000);
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}  
+	  navigateMenu(driver,2,navigateToProduct1);	 
 	  driver.findElement(By.xpath("//*[contains(@href,'House_M_D')]/following-sibling::div[@class='product_buttons']//*[@value='Add To Cart']")).click();
-	  //System.out.println("Purchasing first item completed");
-//	  driver.navigate().refresh();
 	  
-	  //Navigate to DVD > Kids > Up
-	  //System.out.println("Purchasing second item");
+	  try {
+		Thread.sleep(20000);
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
 	  navigateMenu(driver,2,navigateToProduct2);
-	  /*WebElement mainMenu2 = driver.findElement(By.cssSelector("*[href*='DVD'][class='dropdown-toggle']"));
-	  Actions action2 = new Actions(driver);
-	  action2.moveToElement(mainMenu2).build().perform();
-	  driver.findElement(By.cssSelector("*[href*='Kids']")).click();
-	  */
 	  driver.findElement(By.xpath("//*[contains(@href,'Up')]/following-sibling::div[@class='product_buttons']//*[@value='Add To Cart']")).click();
-	  //System.out.println("Purchasing second item completed");
-//	  driver.navigate().refresh();
-	  
-//	  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
-	  
-	  //System.out.println("Purchasing third item");
-	  navigateMenu(driver, 2, navigateToProduct3);
-/*	  WebElement mainMenu3 = driver.findElement(By.cssSelector("*[href*='DVD'][class='dropdown-toggle']"));
-	  Actions action3 = new Actions(driver);
-	  action3.moveToElement(mainMenu3).build().perform();
-	  driver.findElement(By.cssSelector("*[class='dropdown dropdown-megamenu'] *[href*='Classic_Films']")).click();
-*/	  driver.findElement(By.xpath("//*[contains(@href,'James')]/following-sibling::div[@class='product_buttons']//*[@value='Add To Cart']")).click();
-	  //System.out.println("Purchasing third item completed");	 
 
-/*
+	  try {
+		Thread.sleep(20000);
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
+	  navigateMenu(driver, 2, navigateToProduct3);
+      driver.findElement(By.xpath("//*[contains(@href,'James')]/following-sibling::div[@class='product_buttons']//*[@value='Add To Cart']")).click();
+	  
+      try {
+		Thread.sleep(20000);
+	} catch (InterruptedException e) {
+		// TODO Auto-generated catch block
+		e.printStackTrace();
+	}
 	  navigateMenu(driver,2,navigateToProduct4);
-	  driver.findElement(By.xpath("//*[contains(@href,'pid180')]/following-sibling::div[@class='product_buttons']//*[@value='Add To Cart']")).click();
-	 */ 
-	  navigateMenu(driver,1,navigateToProduct5);
+	  try {
+			Thread.sleep(20000);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	  driver.findElement(By.xpath("//*[contains(@href,'EKTORP_TULLSTA')]/following-sibling::div[@class='product_buttons']//*[@value='Add To Cart']")).click();
   }
   
-  @Test(priority=3)
+ // @Test(priority=3)
   public void checkOut()
   {
 	driver.navigate().refresh();
@@ -146,7 +142,7 @@ public class PurchaseItems
 	driver.findElement(By.xpath("//*[@class='top-cart-content']//*[contains(text(),'Checkout')]")).click();
   }
   
-  @Test(priority=4)
+  //@Test(priority=4)
   public void billingShippingAddress()
   {
 	String firstName = driver.findElement(By.xpath("//*[@name='billingInfo[Firstname]']")).getAttribute("value");
@@ -176,7 +172,7 @@ public class PurchaseItems
 
   }
   
- @Test(priority=5)
+ //@Test(priority=5)
   public void billingShippingMethod()
   {
 	  List<WebElement> shippingMethods = driver.findElements(By.xpath("//div[@class ='shipping_method_name']/label"));
@@ -206,7 +202,7 @@ public class PurchaseItems
 	 System.out.println("Your order ID is "+orderId[1]);
   } 
   
- @Test(priority=8)
+ //@Test(priority=8)
   public void zignOut()
   {
 	  driver.findElement(By.xpath("//*[contains(@href,'customer_sign_out')]")).click();
@@ -245,9 +241,11 @@ public class PurchaseItems
 	  Actions action;
       switch (menuLevels) 
       {
-          case 1:  driver.findElement(By.cssSelector("*[class='header-navigation'] *[href*='"+menus[0]+"']")).click();
+          case 1:  /*Waiting.WaitForElement(driver, By.cssSelector("*[class='header-navigation'] *[href*='"+menus[0]+"']"), 20);*/
+        	  	   driver.findElement(By.cssSelector("*[class='header-navigation'] *[href*='"+menus[0]+"']")).click();
                    break;
-          case 2:  mainMenu = driver.findElement(By.cssSelector("*[href*='"+menus[0]+"'][class='dropdown-toggle']"));
+          case 2:  /*Waiting.WaitForElement(driver, By.cssSelector("*[href*='"+menus[0]+"'][class='dropdown-toggle']"), 20);*/
+        	  	   mainMenu = driver.findElement(By.cssSelector("*[href*='"+menus[0]+"'][class='dropdown-toggle']"));
     	  		   action = new Actions(driver);
     	  		   action.moveToElement(mainMenu).build().perform();
     	  		   driver.findElement(By.cssSelector("*[class='dropdown dropdown-megamenu'] *[href*='"+menus[1]+"']")).click();
